bcee3f61c5f021d7abe95e6ae4cda8d97a00d920
Ivan <77185900+wpinrui@users.noreply.github.com>
2026-09-24 18:31:33 +0800

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <main>
+      <h1>scratch-block-png</h1>
+      <p className="caption">
+        Edit <code>src/App.tsx</code> and save to get started.
+      </p>
+    </main>
+  );
+}
